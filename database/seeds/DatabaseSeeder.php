b87358d682b3df8c76f52860dedea69dca71dc39
ZOHAIB <zohaiblab1@gmail.com>
2020-03-22 00:04:26 +0500

    db seeded

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        factory(App\Article::class,3)->create();
+        $tags = factory(App\Tag::class,3)->create();
+        // Populate the pivot table
+        App\Article::all()->each(function ($articles) use ($tags) {
+            $articles->tags()->attach(
+                $tags->random(rand(1, 3))->pluck('id')->toArray()
+            );
+        });
     }
 }
